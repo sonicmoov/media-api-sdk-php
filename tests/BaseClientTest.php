7d14bce7095b531f31c9a67beae1fa6453e1a7c6
thomas <thomas@sonicmoov.com>
2016-12-16 17:43:07 +0900

    add test file

---
+++ tests/BaseClientTest.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__ . "/../src/MediaDivision/SDK/BaseClient.php";
+
+class BaseClientTest extends \PHPUnit_Framework_TestCase
+{
+
+}
---
+++ tests/ClientTest.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__ . "/../src/MediaDivision/SDK/Client.php";
+
+class ClientTest extends \PHPUnit_Framework_TestCase
+{
+
+}
